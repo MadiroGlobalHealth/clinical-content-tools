4cb28f9e07b0317832ddb3d14d935f5c2a04f8df
Michaël Bontyes <michaelbontyes@gmail.com>
2024-08-28 18:32:16 -0400

    Now supporting multiple forms to check

--- checker.py
+++ checker.py
@@ -5,7 +5,16 @@
 import json
 import requests
 import csv
-import time
+
+# List of JSON files
+json_files = [
+    'generated_form_schemas/F01-MHPSS_Baseline.json', 
+    'generated_form_schemas/F02-MHPSS_Follow-up.json', 
+    'generated_form_schemas/F03-mhGAP_Baseline.json',
+    'generated_form_schemas/F04-mhGAP_Follow-up.json',
+    'generated_form_schemas/F05-MH_Closure.json',
+    'generated_form_schemas/F06-PHQ-9.json'
+    ]
 
 def find_key_in_json(data, key):
     found_values = []
@@ -21,32 +30,41 @@ def find_key_in_json(data, key):
             found_values.extend(find_key_in_json(item, key))
     
     return found_values
+
 def main():
-    # Load the json file
-    with open('generated_form_schemas/F01-MHPSS_Baseline.json', 'r') as f:
-        data = json.load(f)
+    # List to store concept status
+    concept_status = []
 
-    # Extract concept keys and UUIDs
-    concept_uuids = find_key_in_json(data, "concept")
+    # Load the json files (Nutrition.json, F01-MHPSS_Baseline.json, etc.)
+    # Create a loop to iterate over all json files
+    for form in json_files:
+        print(f'Processing {form}')
+        with open(form, 'r') as f:
+            data = json.load(f)
 
-    # Update the status of each ID in a CSV file
-    with open('concept_status.csv', 'w', newline='') as csvfile:
-        fieldnames = ['uuid', 'status', 'name']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+            # Extract concept keys and UUIDs
+            concept_uuids = find_key_in_json(data, "concept")
 
-        for concept_key, uuid in concept_uuids:
-            # Make API call to check status and give time to OpenMRS to respond
-            response = requests.get(f'http://localhost/openmrs/ws/rest/v1/concept/{uuid}', auth=('admin', 'Admin123'))
-            if response.status_code == 200:
-                # Get value of key 'display' from response
-                name = response.json().get('display')
-                writer.writerow({'uuid': uuid, 'status': 'Exists', 'name': name})
-                print(f'Concept {name} with UUID {uuid} exists in the OpenMRS database')
-            else:
-                name = 'not found'
-                writer.writerow({'uuid': uuid, 'status': 'Not found'})
-                print(f'Error checking concept with UUID {uuid}: {response.status_code}')
+            for concept_key, uuid in concept_uuids:
+                # Make API call to check status and give time to OpenMRS to respond
+                response = requests.get(f'http://msf-ocg-openmrs3-dev.westeurope.cloudapp.azure.com/openmrs/ws/rest/v1/concept/{uuid}', auth=('admin', 'Admin123'))
+                if response.status_code == 200:
+                    # Get value of key 'display' from response
+                    name = response.json().get('display')
+                    concept_status.append({'form': form, 'uuid': uuid, 'status': 'Exists', 'name': name})
+                    print(f'Concept {name} with UUID {uuid} exists in the OpenMRS database')
+                else:
+                    name = 'not found'
+                    concept_status.append({'form': form, 'uuid': uuid, 'status': 'Not found'})
+                    print(f'Error checking concept with UUID {uuid}: {response.status_code}')
+
+                # Update the status of each ID in a CSV file
+                with open('concept_status.csv', 'w', newline='') as csvfile:
+                    fieldnames = ['form', 'uuid', 'status', 'name']
+                    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writeheader()
+                    writer.writerows(concept_status)
+
     print('Concept status updated successfully!')
 
 if __name__ == '__main__':
